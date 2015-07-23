20daf7312a62a9e701f778874ed0a3a40273691a
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-07-22 21:37:43 -0700

    Add local config overrides

---
+++ local_config_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Author: J. Ryan Rembert
+Project: catalog
+Source: https://github.com/jrrembert/catalog
+
+Copyright (C) 2015 J. Ryan Rembert. All rights reserved.
+
+Redistribution of source code perfectly cool as long as the
+above copyright notice is provided and you don't sue me if
+something (somehow) explodes. Unless it explodes into a
+rainbow of mutant dinosaurs made out of cookie batter.
+Then I assume complete credit.
+""" 
+# Enable for development environment
+DEBUG = True
+
+# Define application directory
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database we are working with
+DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+DATABASE_CONNECT_OPTIONS = {}
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data. 
+CSRF_SESSION_KEY = "secret_csrf"
+
+# Secret key for signing cookies
+SECRET_KEY = "secretsecrets"
