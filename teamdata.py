73866c8d076fda029557ee8fe27e8f70e98063f6
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-08-07 14:37:11 -0700

    Update test data

--- teamdata.py
+++ teamdata.py
@@ -12,6 +12,8 @@ something (somehow) explodes. Unless it explodes into a
 rainbow of mutant dinosaurs made out of cookie batter.
 Then I assume complete credit.
 """
+import datetime
+
 from catalog_project import db
 from catalog_project.models import Sports, Teams, Users
 
@@ -20,13 +22,17 @@ session = db.session()
 
 # Create dummy user
 user1 = Users(name="J. Ryan Rembert", email="rynliquid@gmail.com",
-             picture='https://pbs.twimg.com/profile_images/588416345416404992/y8EvAjvm.jpg')
+              picture='https://pbs.twimg.com/profile_images/588416345416404992/y8EvAjvm.jpg', 
+              created_date=datetime.datetime.now())
 session.add(user1)
 
 # Create sports
-sports = [Sports(user_id=1, name="Baseball"),
-          Sports(user_id=2, name="Football"),
-          Sports(user_id=1, name="Basketball")]
+sports = [Sports(user_id=1, name="Baseball", 
+                 created_date=datetime.datetime.now()),
+          Sports(user_id=2, name="Football", 
+                 created_date=datetime.datetime.now()),
+          Sports(user_id=1, name="Basketball", 
+                 created_date=datetime.datetime.now())]
 session.add_all(sports)
 
 # Commit sports so that sqlite will assign them an id
@@ -34,43 +40,63 @@ session.commit()
 
 # Teams for Baseball
 baseball_teams = [Teams(user_id=1, name="Atlanta Braves", wins=10,
-                        losses=5, league = 'NL', sport_id=sports[0].id),
-                  Teams(user_id=1, name="St. Louis Cardinals", wins=10, losses=5, 
-                        league = 'NL', sport_id=sports[0].id),
-                  Teams(user_id=1, name="Los Angeles Dodgers", wins=10, losses=5, 
-                        league = 'NL', sport_id=sports[0].id),
-                  Teams(user_id=1, name="Texas Rangers", wins=10, losses=5, 
-                        league = 'AL', sport_id=sports[0].id),
-                  Teams(user_id=1, name="Tampa Bay Rays", wins=10, losses=5, 
-                        league = 'AL', sport_id=sports[0].id)]
+                        losses=5, league = 'NL', sport_id=sports[0].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="St. Louis Cardinals", wins=10,
+                        losses=5, league = 'NL', sport_id=sports[0].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="Los Angeles Dodgers", wins=10,
+                        losses=5, league = 'NL', sport_id=sports[0].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="Texas Rangers", wins=10,
+                        losses=5, league = 'AL', sport_id=sports[0].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="Tampa Bay Rays", wins=10,
+                        losses=5, league = 'AL', sport_id=sports[0].id, 
+                        created_date=datetime.datetime.now())]
 
 session.add_all(baseball_teams)
 
 # Teams for Football
 football_teams = [Teams(user_id=1, name="Atlanta Falcons", wins=10, 
-                        losses=5, league = 'AFC', sport_id=sports[1].id),
-                  Teams(user_id=1, name="New England Patriots", wins=10, losses=5, 
-                        league = 'AFC', sport_id=sports[1].id),
-                  Teams(user_id=1, name="Indianapolis Colts", wins=10, losses=5, 
-                        league = 'AFC', sport_id=sports[1].id),
-                  Teams(user_id=1, name="Denver Broncos", wins=10, losses=5, 
-                        league = 'NFC', sport_id=sports[1].id),
-                  Teams(user_id=1, name="Seattle Seahawks", wins=10, losses=5, 
-                         league = 'NFC', sport_id=sports[1].id)]
+                        losses=5, league = 'AFC', sport_id=sports[1].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="New England Patriots", wins=10,
+                        losses=5, league = 'AFC', sport_id=sports[1].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="Indianapolis Colts", wins=10,
+                        losses=5, league = 'AFC', sport_id=sports[1].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="Denver Broncos", wins=10,
+                        losses=5, league = 'NFC', sport_id=sports[1].id, 
+                        created_date=datetime.datetime.now()),
+                  Teams(user_id=1, name="Seattle Seahawks", wins=10,
+                        losses=5, league = 'NFC', sport_id=sports[1].id, 
+                         created_date=datetime.datetime.now())]
 
 session.add_all(football_teams)
 
 # Teams for Basketball
 basketball_teams = [Teams(user_id=2, name="Atlanta Hawks", wins=10, 
-                          losses=5, league = 'Eastern Conference', sport_id=sports[2].id),
+                          losses=5, league = 'Eastern Conference', 
+                          sport_id=sports[2].id, 
+                          created_date=datetime.datetime.now()),
                     Teams(user_id=2, name="Indiana Pacers", wins=10, 
-                          losses=5, league = 'Eastern Conference', sport_id=sports[2].id),
+                          losses=5, league = 'Eastern Conference', 
+                          sport_id=sports[2].id, 
+                          created_date=datetime.datetime.now()),
                     Teams(user_id=2, name="Detroit Pistons", wins=10, 
-                          losses=5, league = 'Eastern Conference', sport_id=sports[2].id),
+                          losses=5, league = 'Eastern Conference', 
+                          sport_id=sports[2].id, 
+                          created_date=datetime.datetime.now()),
                     Teams(user_id=2, name="Los Angeles Clippers", wins=10, 
-                          losses=5, league = 'Western Conference', sport_id=sports[2].id),
+                          losses=5, league = 'Western Conference', 
+                          sport_id=sports[2].id, 
+                          created_date=datetime.datetime.now()),
                     Teams(user_id=2, name="Denver Nuggets", wins=10, 
-                          losses=5, league = 'Western Conference', sport_id=sports[2].id)]
+                          losses=5, league = 'Western Conference', 
+                          sport_id=sports[2].id, 
+                          created_date=datetime.datetime.now())]
 
 session.add_all(basketball_teams)
 session.commit()
